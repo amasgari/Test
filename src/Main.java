7f10b23d723f54bff66d7e66f5ab961e80619f30
Ali <younesi_ali@yahoo.com>
2019-03-06 17:03:20 +0330

    changing Main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int x = 5;
         int y = 6;
-        System.out.println(x + y);
+        System.out.println(x + y * 2);
     }
 }
